dadfe93ccb3312e37cd60ce3c66e7b7539a00e0f
Edgar Méndez <mdzredgar@gmail.com>
2023-02-21 22:05:59 -0600

    Vista del producto iniciada. Funcionalidad de restar y sumar productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Purchases from './pages/Purchases'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Ecommerce</h2> 
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/products/:id' element={<Product />} />
+        <Route path='/purchases' element={<Purchases />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,71 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const Product = () => {
+  const [product, setProduct] = useState()
+  const [quantity, setQuantity] = useState(1)
+
+  const {id} = useParams()
+
+  const handleLess = () => {
+    const newQuantity = quantity -1
+    if(newQuantity >= 1) {
+      setQuantity(newQuantity) 
+    }
+  }
+
+  const handlePlus = () => setQuantity(quantity + 1)
+
+  useEffect(() => {
+    const URL = `https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`
+    axios
+      .get(URL)
+      .then((res) => setProduct(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <main>
+      <section>
+        {/* Parte superior */}
+        <section>
+          <div>
+            <img src={product?.images[0].url} alt="" />
+          </div>
+        </section>
+
+      {/* Parte inferior */}
+      <section>
+        <h4>{product?.brand}</h4>
+        <h3>{product?.title}</h3>
+
+        <div>
+          <div>
+            <h4>Price</h4>
+            <h3>$ {product?.price}</h3>
+          </div>
+
+          <div>
+            <h4>Quantity</h4>
+            <div>
+              <button onClick={handleLess}>-</button>
+              <h4>{quantity}</h4>
+              <button onClick={handlePlus}>+</button>
+            </div>
+          </div>
+        </div>
+
+        <button>
+          Add to cart <i className='bx bx-cart'></i>
+        </button>
+
+        <p>{product?.description}</p>
+      </section>
+
+      </section>
+    </main>
+  )
+}
+
+export default Product
