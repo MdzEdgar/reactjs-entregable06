711da6242a462019a8806fa15ca6f273185afed0
Edgar Méndez <mdzredgar@gmail.com>
2023-02-27 17:33:45 -0600

    Creación de filtrado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,16 @@ import "./styles/Home.css"
 
 const Home = () => {
   const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  const [nameFilter, setNameFilter] = useState("")
+  const [filterProducts, setFilterProducts] = useState([])
+  const [categoryFilter, setCategoryFilter] = useState(0)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const nameProduct = e.target.nameProduct.value
+    setNameFilter(nameProduct)
+  }
 
   useEffect(() => {
     axiosEcommerce
@@ -13,15 +23,58 @@ const Home = () => {
       .catch((err) => console.log(err))
   }, [])
 
+  useEffect(() => {
+    axiosEcommerce
+      .get("/categories")
+      .then((res) => setCategories(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    const newProductsByName = products.filter((product) => 
+    product.title.toLowerCase().includes(nameFilter.toLowerCase())
+    )
+    if(categoryFilter){
+      const newProductsByCategory = newProductsByName.filter(
+        product => product.categoryId === categoryFilter
+        )
+      setFilterProducts(newProductsByCategory)
+    } else {
+      setFilterProducts(newProductsByName)
+    }
+  }, [nameFilter, products, categoryFilter])
+
   return (
-    <main className='home'>
-      <section className='home__listProducts'>
-        {products.map(product => (
-          <ProductCard  key={product.id}  product={product}/>
+    <main className="home">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input id="nameProduct" type="text" />
+          <button>
+            <i className="bx bx-search"></i>
+          </button>
+        </div>
+        <div>
+          <h3>Categories</h3>
+          <ul>
+            <li onClick={() => setCategoryFilter(0)} >All</li>
+            {categories.map((category) => (
+              <li
+                onClick={() => setCategoryFilter(category.id)}
+                key={category.id}
+              >
+                {category.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </form>
+      <section className="home__listProducts">
+        {filterProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
         ))}
       </section>
     </main>
-  )
+  );
 }
 
 export default Home
