e6bd4b0dbed9b8f8668a51ce870afaf88e326ffc
Edgar Méndez <mdzredgar@gmail.com>
2023-02-27 00:21:23 -0600

    Funcionalidad del entregable termianda.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,18 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import ProductCard from '../components/Home/ProductCard'
+import { addProductCart } from '../store/slices/cart.slice'
 import { axiosEcommerce } from '../utils/configAxios'
 
 const Product = () => {
   const [product, setProduct] = useState()
   const [quantity, setQuantity] = useState(1)
+  const [similarProducts, setsimilarProducts] = useState([])
 
   const {id} = useParams()
+
+  const dispatch = useDispatch()
 
   const handleLess = () => {
     const newQuantity = quantity -1
@@ -18,12 +23,38 @@ const Product = () => {
 
   const handlePlus = () => setQuantity(quantity + 1)
 
+  const handleClickAddProduct = () => {
+    const data = {
+      quantity,
+      productId: product?.id
+    }
+    dispatch(addProductCart(data))
+  }
+
   useEffect(() => {
     axiosEcommerce
       .get(`/products/${id}`)
       .then((res) => setProduct(res.data))
       .catch((err) => console.log(err))
-  }, [])
+  }, [id])
+
+  useEffect(() => {
+    if(product){
+      axiosEcommerce
+        .get(`/products?categoryId=${product?.categoryId}`)
+        .then((res) => {
+          const newSimilarProducts = res.data.filter(
+            (productByCategory) => productByCategory !== product.id
+          )
+          setsimilarProducts(newSimilarProducts)
+        })
+        .catch((err) => console.log(err))
+    }
+  }, [product])
+
+  useEffect(() => {
+    setQuantity(1)
+  }, [id])
 
   return (
     <main>
@@ -35,37 +66,44 @@ const Product = () => {
           </div>
         </section>
 
-      {/* Parte inferior */}
-      <section>
-        <h4>{product?.brand}</h4>
-        <h3>{product?.title}</h3>
-
-        <div>
-          <div>
-            <h4>Price</h4>
-            <h3>$ {product?.price}</h3>
-          </div>
+        {/* Parte inferior */}
+        <section>
+          <h4>{product?.brand}</h4>
+          <h3>{product?.title}</h3>
 
           <div>
-            <h4>Quantity</h4>
             <div>
-              <button onClick={handleLess}>-</button>
-              <h4>{quantity}</h4>
-              <button onClick={handlePlus}>+</button>
+              <h4>Price</h4>
+              <h3>$ {product?.price}</h3>
+            </div>
+
+            <div>
+              <h4>Quantity</h4>
+              <div>
+                <button onClick={handleLess}>-</button>
+                <h4>{quantity}</h4>
+                <button onClick={handlePlus}>+</button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <button>
-          Add to cart <i className='bx bx-cart'></i>
-        </button>
+          <button onClick={handleClickAddProduct}>
+            Add to cart <i className="bx bx-cart"></i>
+          </button>
 
-        <p>{product?.description}</p>
+          <p>{product?.description}</p>
+        </section>
       </section>
 
+      <h2>Discover similar items</h2>
+
+      <section>
+        {similarProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </section>
     </main>
-  )
+  );
 }
 
 export default Product
