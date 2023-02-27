63694bc75208cf27fb5b6cfe2577ae77a29b4a95
Edgar Méndez <mdzredgar@gmail.com>
2023-02-27 00:10:39 -0600

    Correcion de ProductCard

--- src/components/Home/ProductCard.jsx
+++ src/components/Home/ProductCard.jsx
@@ -9,14 +9,14 @@ const ProductCard = ({product}) => {
   const dispatch = useDispatch()
 
   const handleClickProduct = () => {
-    navigate(`/products/${product.id}`)
+    navigate(`/products/${product?.id}`)
   }
 
   const handleClickAddProduct = (e) => {
     e.stopPropagation()
     const data = {
       quantity: 1,
-      productId: product.id
+      productId: product?.id
     }
     dispatch(addProductCart(data))
   }
@@ -25,15 +25,15 @@ const ProductCard = ({product}) => {
     <article onClick={handleClickProduct}>
       <header>
         <div>
-          <img src={product.images[0].url} alt="" />
+          <img src={product?.images[0].url} alt="" />
         </div>
       </header>
       <section>
-        <h4>{product.brand}</h4>
-        <h3>{product.title}</h3>
+        <h4>{product?.brand}</h4>
+        <h3>{product?.title}</h3>
 
         <h4>Price</h4>
-        <h3>$ {product.price}</h3>
+        <h3>$ {product?.price}</h3>
         <button onClick={handleClickAddProduct}>
           <i className='bx bx-cart'></i>
         </button>
