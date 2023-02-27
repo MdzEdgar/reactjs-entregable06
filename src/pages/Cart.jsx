7abd8ad12ec1c697c6f8ee260ba20a3c7cf984d8
Edgar Méndez <mdzredgar@gmail.com>
2023-02-26 22:17:18 -0600

    Renombrado en CartProduct

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import CartProduct from '../components/Cart/CartProduct'
 import { getAllCartProducts } from '../store/slices/cart.slice'
 
 const Cart = () => {
+  const {products} = useSelector(store => store.cart)
 
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(getAllCartProducts())
   }, [])
+
   return (
     <main>
-      
+      <section>
+        {products.map((product) => <CartProduct key={product.id} product={product} />)
+        }
+        
+      </section>
     </main>
   )
 }
