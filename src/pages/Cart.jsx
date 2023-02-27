c37053c6fc95427c26b218d537835d2cda14612a
Edgar Méndez <mdzredgar@gmail.com>
2023-02-26 19:40:11 -0600

    Inicio de Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getAllCartProducts } from '../store/slices/cart.slice'
 
 const Cart = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllCartProducts())
+  }, [])
   return (
-    <div>Cart</div>
+    <main>
+      
+    </main>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userInfo from "./slices/userInfo.slice"
+import cart from "./slices/cart.slice"
 
 export default configureStore({
   reducer: {
-    userInfo
+    userInfo,
+    cart
   }
 })
